a73e116700968c182dadd352ad6e8a63b642c5b7
asamanta <ajit.samanta@cbre.com>
2018-06-15 16:04:11 +0530

    JSON Media Formatter added

--- TestAPI/TestAPI/App_Start/WebApiConfig.cs
+++ TestAPI/TestAPI/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Net.Http.Headers;
 
 namespace TestAPI
 {
@@ -10,6 +11,9 @@ namespace TestAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("text/html"));
+
 
             // Web API routes
             config.MapHttpAttributeRoutes();
